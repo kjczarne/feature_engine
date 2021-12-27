4f91276a9b2e1d2583341013a14c0b04568258e1
Krzysztof Czarnecki <krzysztof.czarnecki@technica-engineering.de>
2021-12-27 12:17:05 +0100

    feat: CLI app starter

--- feature_engine/main.py
+++ feature_engine/main.py
@@ -1,36 +1,20 @@
 import argparse
-import yaml
 import dash
-import networkx as nx
 
-from typing import Generator
-from feature_engine.model import FeatureContainer, Feature, Relationship, RelationshipTypes, Direction, DirectionalRelationship, BindingTime
+from pathlib import Path
+from feature_engine.model import FeatureContainer
+
 
 def main():
-    # parser = argparse.ArgumentParser(prog='feature_engine', description='Feature Engineering Toolchain')
-    # parser.add_argument('-i', '--input', help='input', type=str)
-    # parser.add_argument('-o', '--output', help='output', type=str)
+    parser = argparse.ArgumentParser(prog='feature_engine', description='Feature Engineering Toolchain')
+    parser.add_argument('-i', '--input', help='input', type=str, required=True)
 
-    # args = parser.parse_args()
+    args = parser.parse_args()
 
-    # G = nx.Graph()
+    FC = FeatureContainer.from_yaml(Path(args.input), debug=True)
 
-    # nx.draw_networkx(G)
-    
-    FC = FeatureContainer(features=[
-        Feature(0, "Feature 1", BindingTime.COMPILATION, "Rationale 1", True, [Relationship(1, RelationshipTypes.ALTERNATIVE)]),
-        Feature(1, "Feature 2", BindingTime.COMPILATION, "Rationale 2", True, []),
-    ])
-    
-    FC.to_yaml("test.yaml")
-    G = FC.to_nx_graph()
-    
-    FC2 = FeatureContainer.from_yaml("test2.yaml")
-    G2 = FC2.to_nx_graph()
-    print("test")
-    
-    app = dash.Dash(__name__)
-    app.layout = FC2.cytoscape()
+    app = dash.Dash(__name__, title="Feature Engine")
+    app.layout = FC.cytoscape()
     app.run_server(debug=True)
 
 
